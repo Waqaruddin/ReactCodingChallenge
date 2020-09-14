ce6869b4ee83ccb2894e88c4acbc3796599c4ca3
Waqaruddin <syedwaqar.uddin@outlook.com>
2020-09-14 13:22:42 -0400

    Uploaded Counter challenge

--- App.js
+++ App.js
@@ -1,11 +1,30 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, {useState} from 'react';
+import { Button, StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
+
+  const [counter, setCounter] = useState(0);
+
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+
+      <Button
+
+      color = "#FF8C00"
+      
+      style ={styles.button1} title = "INCREMENT" 
+      onPress = {() => {setCounter(counter+1)}}
+      />
+      
+
+      <Button
+      style = {styles.button2}
+      title = "DECREMENT"
+      onPress = {() => {setCounter(counter-1)}}/>
+
+      <Text> Counter =  {counter}</Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -18,4 +37,21 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+
+  button1:{
+    paddingTop:10,
+    paddingBottom:10,
+    marginTop:15,
+    marginRight:40,
+    borderWidth:1,
+    fontWeight:'bold'
+  },
+
+  button2:{
+    paddingTop:10,
+    paddingBottom:10,
+    marginTop:15,
+    marginRight:40,
+    borderWidth:1
+  }
 });
